f0dadf7a646b8a9e6ff69c181ce6336a0edea507
jakirock4 <jakirock4@hotmail.com>
2021-10-19 19:10:15 -0500

    Se añade eslint al proyecto

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,4 @@
 export function format(first: string, middle: string, last: string): string {
+  // eslint-disable-next-line @stencil/strict-boolean-conditions
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
